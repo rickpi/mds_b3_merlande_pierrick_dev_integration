fd12ed644fc4e0f819983b4ade5bf70a1e343496
Pierrick MERLANDE <merlandepierrick@gmail.com>
2021-01-21 15:10:35 +0100

    test: should return all colors

--- test/test.js
+++ test/test.js
@@ -11,16 +11,31 @@ chai.use(chaiHttp)
   * Génération des nouvelles couleurs et enregistrement de ces
   * couleurs dans un tableau.
   */
-const newValues = []
-const colorKey = 'NEW_COLOR_'
-let nextCursor = 0;
-const payloadColor = () => {
-  const nextColor = `${colorKey}${nextCursor}`
-  newValues.push(nextColor)
-  nextCursor++;
-  return { 'color': nextColor }
-}
-const getCurrentCulor = () => {
-  return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
-}
-// <-- FIN
+ const newValues = []
+ const colorKey = 'NEW_COLOR_'
+ let nextCursor = 0;
+ const payloadColor = () => {
+   const nextColor = `${colorKey}${nextCursor}`
+   newValues.push(nextColor)
+   nextCursor++;
+   return { 'color': nextColor }
+ }
+ const getCurrentColor = () => {
+   return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
+ }
+ // <-- FIN
+
+describe('GET /colors', () => {
+  it('should return all colors', (done) => {
+    chai.request(app)
+      .get('/colors')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.an('object');
+        res.body.results.should.be.an('array');
+        res.body.results.should.be.eql(['RED', 'GREEN', 'BLUE']);
+        done();
+      });
+  });
+});
